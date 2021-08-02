52ca616445165f3157075d6a8faa968b64f176af
someya-takashi <someya.takashi.inter@gmail.com>
2021-08-03 01:20:55 +0900

    commit

--- test.py
+++ test.py
@@ -1,60 +1,25 @@
-import sys
-sys.setrecursionlimit(100000000)
+N = int(input())
 
-B = [list(map(int, input().split())) for _ in range(2)]
-C = [list(map(int, input().split())) for _ in range(3)]
+mod = 10**9+7
 
-def points(g):
-    chokudai = 0
-    for i in range(2):
-        for j in range(3):
-            if g[i][j] == g[i+1][j]:
-                chokudai += B[i][j]
-    
-    for i in range(3):
-        for j in range(2):
-            if g[i][j] == g[i][j+1]:
-                chokudai += C[i][j]
+class RepeatedPermutation():
+  fct, inv = [], []
+  mod = 10**9+7
+  def __init__(self, imx, imod) -> None:
+    self.mod = imod
+    self.fct, self.inv = [1] * (imx+2), [1]* (imx+2)
+    for i in range(2, imx+1): self.fct[i] = self.fct[i-1] * i % imod
+    self.inv[imx] = pow(self.fct[imx], imod-2, imod)
+    for i in range(imx, 0,-1): self.inv[i-1] = self.inv[i] * i % imod
+  def pwr(self, n1, n2):
+    return self.fct[n1+n2] * self.inv[n1] % self.mod  *self.inv[n2] % self.mod
 
-    return chokudai
+p = RepeatedPermutation(N, mod)
 
-def dfs(turn):
-    if turn == 10:
-        return points(grid)
-    
-    if turn % 2 == 1:
-        ret = -10**10
-        for i in range(3):
-            for j in range(3):
-                if grid[i][j] == "":
-                    grid[i][j] = "o"
-                    ret = max(ret, dfs(turn+1))
-                    grid[i][j] = ""
-        
-        return ret
-    
-    else:
-        ret = 10**10
-        for i in range(3):
-            for j in range(3):
-                if grid[i][j] == "":
-                    grid[i][j] = "x"
-                    ret = min(ret, dfs(turn+1))
-                    grid[i][j] = ""
-        
-        return ret
-
-grid = [[""] * 3 for _ in range(3)]
-
-ans = dfs(1)
-
-s = 0
-for i in range(2):
-    for j in range(3):
-        s += B[i][j]
-for i in range(3):
-    for j in range(2):
-        s += C[i][j]
+ans = 0
+ans += p.pwr(1,1)
+ans += p.pwr(1,2)
+ans += p.pwr(2,1)
+ans += p.pwr(2,2)
 
 print(ans)
-print(s-ans)
--- test2.py
+++ test2.py
@@ -1,158 +1,29 @@
-# segfuncと単位元を目的に応じて書き換える
-# https://qiita.com/takayg1/items/b7b3f7d458915bcc7a4e
-
-import sys
-input = sys.stdin.readline
-
-#####segfunc#####
-def segfunc(x, y):
-    return min(x, y)
-#################
-
-#####ide_ele#####
-ide_ele = 10**17
-#################
-
-class LazySegmentTree:
-    """
-    init(init_val, ide_ele): 配列init_valで初期化 O(N)
-    add(l, r, x): 区間[l, r)にxを加算 O(logN)
-    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
-    """
-    def __init__(self, init_val, segfunc, ide_ele):
-        """
-        init_val: 配列の初期値
-        segfunc: 区間にしたい操作
-        ide_ele: 単位元
-        num: n以上の最小の2のべき乗
-        data: 値配列(1-index)
-        lazy: 遅延配列(1-index)
-        """
-        n = len(init_val)
-        self.segfunc = segfunc
-        self.ide_ele = ide_ele
-        self.num = 1 << (n - 1).bit_length()
-        self.data = [ide_ele] * 2 * self.num
-        self.lazy = [0] * 2 * self.num
-        # 配列の値を葉にセット
-        for i in range(n):
-            self.data[self.num + i] = init_val[i]
-        # 構築していく
-        for i in range(self.num - 1, 0, -1):
-            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
-
-    def gindex(self, l, r):
-            """
-            伝搬する対象の区間を求める
-            lm: 伝搬する必要のある最大の左閉区間
-            rm: 伝搬する必要のある最大の右開区間
-            """
-            l += self.num
-            r += self.num
-            lm = l >> (l & -l).bit_length()
-            rm = r >> (r & -r).bit_length()
-
-            while r > l:
-                if l <= lm:
-                    yield l
-                if r <= rm:
-                    yield r
-                r >>= 1
-                l >>= 1
-            while l:
-                yield l
-                l >>= 1
-
-    def propagates(self, *ids):
-        """
-        遅延伝搬処理
-        ids: 伝搬する対象の区間 
-        """
-        for i in reversed(ids):
-            v = self.lazy[i]
-            if not v:
-                continue
-            self.lazy[2 * i] += v
-            self.lazy[2 * i + 1] += v
-            self.data[2 * i] += v
-            self.data[2 * i + 1] += v
-            self.lazy[i] = 0
-
-    def add(self, l, r, x):
-        """
-        区間[l, r)の値にxを加算
-        l, r: index(0-index)
-        x: additional value
-        """
-        *ids, = self.gindex(l, r)
-        l += self.num
-        r += self.num
-        while l < r:
-            if l & 1:
-                self.lazy[l] += x
-                self.data[l] += x
-                l += 1
-            if r & 1:
-                self.lazy[r - 1] += x
-                self.data[r - 1] += x
-            r >>= 1
-            l >>= 1
-        for i in ids:
-            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1]) + self.lazy[i]
+from sys import setrecursionlimit
+from functools import lru_cache
 
 
-    def query(self, l, r):
-        """
-        [l, r)のsegfuncしたものを得る
-        l: index(0-index)
-        r: index(0-index)
-        """
-        *ids, = self.gindex(l, r)
-        self.propagates(*ids)
+@lru_cache(maxsize=None)
+def f(i, r):
+    if i == N:
+        return r == 0
 
-        res = self.ide_ele
-
-        l += self.num
-        r += self.num
-        while l < r:
-            if l & 1:
-                res = self.segfunc(res, self.data[l])
-                l += 1
-            if r & 1:
-                res = self.segfunc(res, self.data[r - 1])
-            l >>= 1
-            r >>= 1
-        return res
-
-    def LessL(self, l, r, a):
-        stack = [(0, 0, self.num)]
-        while stack:
-            u, ul, ur = stack.pop()
-            if ur-ul == 1: return u-self.num+1
-            um = (ul+ur)//2
-            if self.data[u*2+2] < a and um < r and l < ur:
-                stack.append((u*2+2, um, ur))
-            if self.data[u*2+1] < a and ul < r and l < um:
-                stack.append((u*2+1, ul, um))
-        return -1
+    if X[i] == 'A':
+        if not f(i + 1, r * 10 % 7):
+            return False
+        return f(i + 1, (r * 10 + int(S[i])) % 7)
+    elif X[i] == 'T':
+        if f(i + 1, r * 10 % 7):
+            return True
+        return f(i + 1, (r * 10 + int(S[i])) % 7)
 
 
-Q = int(input())
+setrecursionlimit(10 ** 6)
 
-H = [ide_ele] * Q
-sg = LazySegmentTree(H, segfunc, ide_ele)
-temp = 0
+N = int(input())
+S = input()
+X = input()
 
-for i in range(Q):
-    q = list(map(int, input().split()))
-    if q[0] == 1:
-        sg.add(i, i+1,q[1] - temp - ide_ele)
-    elif q[0] == 2:
-        sg.add(0, Q+1, q[1])
-        temp += q[1]
-    else:
-        ans = sg.query(0, Q)
-        id = sg.LessL(0, Q, ans+1)
-        # sg.data[id+sg.num-1] = ide_ele
-        sg.add(id-1, id, ide_ele-ans)
-        print(ans)
+if f(0, 0):
+    print("Takahashi")
+else:
+    print("Aoki")
